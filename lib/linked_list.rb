74201ba97e868bf742538ba5d732a93603f1392a
Gabrielle Epstein <doctorgabi@gmail.com>
2013-12-12 15:23:55 -0600

    Passes first 6 tests.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,10 +1,21 @@
 class LinkedList
-  # attr_accessor :payload
+
   def add_item(payload)
-    @payload = payload
+    if @first_node.nil?
+      @first_node = LinkedListItem.new(payload)
+    else
+      # loop across your list, find which one is last, add to that,
+      @first_node.next_list_item = LinkedListItem.new(payload)
+    end
+      # use last? to get to the last one and then add the next item
   end
+
   def get(index)
-    @payload
+    current_node = @first_node
+    index.times do
+      current_node = current_node.next_list_item
+    end
+    current_node.payload
   end
 
 end
