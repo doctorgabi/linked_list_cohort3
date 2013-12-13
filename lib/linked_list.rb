3e8989a929137b492d40987a38ae1ac287ab7f44
Gabrielle Epstein <doctorgabi@gmail.com>
2013-12-12 21:00:26 -0600

    18 of 63 linked list tests now pass.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,21 +1,31 @@
 class LinkedList
-
   def add_item(payload)
     if @first_node.nil?
       @first_node = LinkedListItem.new(payload)
     else
-      # loop across your list, find which one is last, add to that,
-      @first_node.next_list_item = LinkedListItem.new(payload)
+      last_node = @first_node
+      while last_node.last? == false
+        last_node = last_node.next_list_item
+      end
+      last_node.next_list_item = LinkedListItem.new(payload)
     end
-      # use last? to get to the last one and then add the next item
   end
+
 
   def get(index)
-    current_node = @first_node
-    index.times do
-      current_node = current_node.next_list_item
+    counter = 0
+    if index < 0
+      raise IndexError
+    else
+      current_node = @first_node
+      index.times do
+        if current_node == nil
+          raise IndexError
+        else
+          current_node = current_node.next_list_item
+        end
+      end
+      current_node.payload
     end
-    current_node.payload
   end
-
 end
